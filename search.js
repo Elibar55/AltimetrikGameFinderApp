fb2ce8e9695ea0bb5c3414ef9139eda8c8ac27ba
Elibar55 <87397616+Elibar55@users.noreply.github.com>
2022-07-07 16:46:59 -0300

    search update
    corrected the search bar behavior so now it uses the auto complete box and the loads the selected game data to the main container

--- search.js
+++ search.js
@@ -1,5 +1,8 @@
+const autoCompleteBox = document.getElementById('auto-complete')
+
+
 const searchGame = async (query) => {
-  const url = `https://api.rawg.io/api/games?key=d16525c19948468798732d35e4657b48&search=${query}`
+  const url = `https://api.rawg.io/api/games?key=d16525c19948468798732d35e4657b48&search=${query}&page_size=4`
   fetch(url)
     .then((response) => response.json())
     .then(({ results }) => {
@@ -16,39 +19,60 @@ const searchGame = async (query) => {
 
 const renderResults = (data) => {
   for (let e = 0; e < data.length; e++) {
-    body += `
-         <div class="card">
-         <div class="game-image">
-           <img class="card-img" src=${data[e].background_image} alt="" />
-         </div>
-         <div class="game-info">
-         
-           <div class="game-title">${data[e].name}</div>
-           <div class="game-number">#${e + 1}</div>
-           
-           </div>
-           <div class="game-stats">
-           <div class="game-release">
-             <div>Release date:</div>
-             <div class="game-data">${data[e].released}</div>
-             <div class="platforms">
-             ${data[e].parent_platforms.map(
-               (p) => `<img src="/assets/platforms/${p.platform.name}.svg"/>`,
-             )}
-              
-            </div>
-           </div>
-           <div class="game-genres">
-             <div>Genres:</div>
-            <div class="game-data">${data[e].genres.map(
-              (g) => `<div class="genres-data">${g.name}</div> `,
-            )}</div>
-           </div>
-            
-         </div>
-       </div>`
+    body += `         
+           <div id="${data[e].id}" class="search-results" >${data[e].name}</div>
+
+          `
   }
-  document.getElementById('container').innerHTML = body
+  autoCompleteBox.innerHTML = body
+
+
+  var resultsRendered = document.querySelectorAll('.search-results')
+  resultsRendered.forEach((resultsRendered) => {
+    resultsRendered.addEventListener('click', () => {
+      autoCompleteBox.classList.add('hidden')
+      fetch(
+        `https://api.rawg.io/api/games/${resultsRendered.id}?key=d16525c19948468798732d35e4657b48`,
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          
+          const container = document.getElementById('container')
+          container.innerHTML = ""
+           container.innerHTML += `  <div class="card small-card">
+        <img src=${res.background_image} alt="" />
+        <div class="card-data">
+          <div class="card-title">
+            <div class="name card-name-small">${res.name}</div>
+            <div class="card-number">#1</div>
+          </div>
+          <div class="card-info">
+          <div class="specs card-specs-small">
+            <div class="release">
+              Release date:
+              <strong class="data-realeased">${res.released}</strong>
+            </div>
+            <div class="genres">
+              Genres:
+              <strong class="data-genres">${res.genres.map(
+                (g) => `${g.name}`,
+              )}</strong>
+            </div>
+          </div>
+          <div class="consoles">${res.parent_platforms.map(
+            (p) =>
+              `<img class="platforms" src="/assets/platforms/${p.platform.name}.svg"/>`,
+          )}</div>
+        </div>
+          <div class="card-description hidden">
+          ${res.description}
+          </div>
+        </div>
+      </div>`
+     
+        })
+    })
+  })
 }
 
 let searchTimeoutToken = 0
@@ -59,8 +83,12 @@ window.onload = () => {
     clearTimeout(searchTimeoutToken)
 
     if (searchFieldElement.value.trim().length === 0) {
+      autoCompleteBox.classList.add('hidden')
       return
+    } else {
+      autoCompleteBox.classList.remove('hidden')
     }
+
     searchTimeoutToken = setTimeout(() => {
       searchGame(searchFieldElement.value)
     }, 250)
